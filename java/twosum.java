957ba52ea33b6e55934754241f8ee6b600964772
Chotle <60724357+lemac2002@users.noreply.github.com>
2022-02-28 12:52:33 -0500

    Update twosum.java

--- java/twosum.java
+++ java/twosum.java
@@ -1,1 +1,19 @@
-aaa
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> a = new  HashMap<Integer, Integer>();
+        int[] result = new int[2];
+        for(int i = 0; i < nums.length; i++){
+            a.put(nums[i], i);
+        }
+        for(int i = 0; i < nums.length; i++){
+            int m = target - nums[i];
+            if(a.containKey(m)){
+                result[0] = i;
+                result[1] = a.get(m);
+                return result;
+            }
+        }
+        return null;
+            
+    }
+}
